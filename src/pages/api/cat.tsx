53d26f66b4e95e99bf05227c3e2b5782bc07f042
Alaa-Magdy <alaa_magdy@outlook.com>
2024-04-01 16:33:29 +0400

    update the apis

---
+++ src/pages/api/cat.tsx
@@ -0,0 +1,74 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type Cat = {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+    description: string;
+    videoPath: string;
+};
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Cat[]>
+    ) 
+    {
+    res.status(200).json([
+        {
+            src: '/cats/cat1.jpg',
+            alt: 'cat1',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 1 cat',
+            videoPath: '/cats/catVideo.mp4'
+        },
+        {
+            src: '/cats/cat2.jpg',
+            alt: 'cat2',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 2 cat',
+            videoPath: '/cats/catVideo.mp4'
+        },
+        {
+            src: '/cats/cat3.jpg',
+            alt: 'cat3',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 3 cat',
+            videoPath: '/cats/catVideo.mp4'
+        },
+        {
+            src: '/cats/cat4.jpg',
+            alt: 'cat4',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 4 cat',
+            videoPath: '/cats/catVideo.mp4'
+        },
+        {
+            src: '/cats/cat5.jpg',
+            alt: 'cat5',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 5 cat',
+            videoPath: '/cats/catVideo.mp4'
+        },
+        {
+            src: '/cats/cat6.jpg',
+            alt: 'cat6',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 6 cat',
+            videoPath: '/cats/catVideo.mp4'
+        },
+        {
+            src: '/cats/cat7.jpg',
+            alt: 'cat7',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 7 cat',
+            videoPath: '/cats/catVideo.mp4'
+        }
+    ]);
+}
---
+++ src/pages/api/horse.tsx
@@ -0,0 +1,74 @@
+// pages/api/dog.js
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type Horse = {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+    description: string;
+    videoPath: string;
+};
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Horse[]>
+    ) {
+    res.status(200).json([
+        {
+            src: '/horses/horse1.jpg',
+            alt: 'horse1',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 1 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        },
+        {
+            src: '/horses/horse2.jpg',
+            alt: 'horse2',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 2 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        },
+        {
+            src: '/horses/horse3.jpg',
+            alt: 'horse3',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 3 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        },
+        {
+            src: '/horses/horse4.jpg',
+            alt: 'horse4',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 4 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        },
+        {
+            src: '/horses/horse5.jpg',
+            alt: 'horse5',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 5 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        },
+        {
+            src: '/horses/horse6.jpg',
+            alt: 'horse6',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 6 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        },
+        {
+            src: '/horses/horse7.jpg',
+            alt: 'horse7',
+            width: 500,
+            height: 500,
+            description: 'these id the description of the 7 horse',
+            videoPath: '/horses/horseVideo.mp4'
+        }
+    ]);
+}
